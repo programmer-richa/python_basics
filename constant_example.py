eb5b39ce71a1d324a1e1c32645250d570cc99b59
programmer-richa <programmer.richa@gmail.com>
2020-04-01 22:17:38 +0530

    Constants in Python

---
+++ constant_example.py
@@ -0,0 +1,9 @@
+import math
+
+# returning the value of const. pi
+print("The value of const. pi is : ", end="")
+print(math.pi)
+
+# returning the value of const. e
+print("The value of const. e is : ", end="")
+print(math.e)
